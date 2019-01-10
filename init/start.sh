0f83c561b126fdc3a05c8e6f7c75fc1162dcc247
Yin <ilona@ilonadeMacBook-Pro.local>
2019-01-11 00:16:01 +0800

    update startup script

--- init/start.sh
+++ init/start.sh
@@ -6,13 +6,15 @@ if [ -n $1 ]; then
     PROJECT_DIR=~/go/src/$1
 fi
 
+rm -rf ${PROJECT_DIR}
+go get -u github.com/yinzhinyy/talk-to-ilona
+go install $1
+
 sudo mkdir -p /etc/talk
 sudo cp ${PROJECT_DIR}/configs/* /etc/talk/
 
 sudo mkdir -p /usr/lib/systemd/system
 sudo cp ${PROJECT_DIR}/init/talk.service /usr/lib/systemd/system/
 sudo systemctl daemon-reload
-
-go install $1
 
 sudo systemctl restart talk
